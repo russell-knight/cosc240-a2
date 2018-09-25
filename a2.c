b6622c64f1159b52430b0f8bcb1558e5c395c759
Russell Knight <rknigh22@myune.edu.au>
2018-09-25 16:19:00 +1000

    Added findWord function

--- a2.c
+++ a2.c
@@ -5,8 +5,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-char word[50];
+char word[100];
+
+void findWord(char *word , char *file){
+    char line[1024];
+    FILE* fp = fopen(file, "r");
+    while (fgets(line , sizeof(line) , fp )!= NULL)
+    {
+        if (strstr(line , word )!= NULL)
+        {
+            printf("%s\n", file);
+            break;
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
 
@@ -18,7 +32,8 @@ int main(int argc, char* argv[]) {
 
     strcpy (word,argv[1]); // assign search word to global variable
 
-    for (int i = 2; i < argc; i++) {
+    // This for loop checks if the given files exist
+    /*for (int i = 2; i < argc; i++) {
 
         int result;
         const char *filename = argv[i];
@@ -30,7 +45,11 @@ int main(int argc, char* argv[]) {
         else {
             printf("ERROR: %s doesn't exist!\n",filename);
         }
-    }
+    }*/
+
+    findWord("hello", "file1.txt");
+    findWord("hello", "file2.txt");
+    findWord("hello", "file3.txt");
 
 
 
